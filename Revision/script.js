150b5129e86b7902f4ec5415800630e45d118075
Priya Mehta <pmehta12122005@gmail.com>
2025-06-18 08:24:09 +0530

    Revision

---
+++ Revision/script.js
@@ -0,0 +1,11 @@
+let firstName ='Anurag'
+let lastName = 'Singh'
+let age = 25
+let isHappy = true
+firstName = 'Harsh'
+let a
+a = 'Priya'
+
+const hourInDay = 24
+
+let userIntro = 'Hi, my name is ' + a + ' ' +lastName + '. I am ' + age + ' ' + 'years old'
