12203582703ed379ba2e8a54c58f858e27e2810a
Priya Mehta <pmehta12122005@gmail.com>
2025-06-19 15:00:40 +0530

    if statement

--- Revision/ep-12_decision-making-in-javascript/script.js
+++ Revision/ep-12_decision-making-in-javascript/script.js
@@ -1,39 +1,39 @@
-const inputName = prompt('Enter your Name') 
-const inputAge = parseInt(+prompt('Enter your Age')) 
-const inputGender = prompt('Male or Female') 
+const userName = prompt('Enter your Name') 
+const userAge = parseInt(+prompt('Enter your Age')) 
+const userGender = prompt('Male or Female') 
 
-console.log(`Name: ${inputName}`)
-console.log(`Age: ${inputAge}`)
-console.log(`Gender: ${inputGender}`)
+console.log(`Name: ${userName}`)
+console.log(`Age: ${userAge}`)
+console.log(`Gender: ${userGender}`)
 
 
-if (inputAge>=0 && inputAge<=5)
+if (userAge>=0 && userAge<=5)
 {
-    console.log(`${inputName} is a kid👶🏻`)
+    console.log(`${userName} is a kid👶🏻`)
 }
-if (inputAge>=6 && inputAge<=18)
+if (userAge>=6 && userAge<=18)
 {
-    console.log(`${inputName} is a school student🧑🏻‍🎓👨🏻‍🎓👩🏻‍🎓`)
+    console.log(`${userName} is a school student🧑🏻‍🎓👨🏻‍🎓👩🏻‍🎓`)
     console.log('He/She is aspiring to become a web developer in future')
     
 }
 
-if (inputAge>=19 && inputAge<=24)
+if (userAge>=19 && userAge<=24)
 {
-    console.log(`${inputName}  is a college student🎓🎓🎓`)
+    console.log(`${userName}  is a college student🎓🎓🎓`)
     console.log('He/She is learning Computer Science') 
 }
     
 
 
-if (inputAge>=25 && inputAge<=45)
+if (userAge>=25 && userAge<=45)
 {
-    console.log(`${inputName} is a Working Professional👨🏻‍💻👩🏻‍💻🧑🏻‍💻`)
+    console.log(`${userName} is a Working Professional👨🏻‍💻👩🏻‍💻🧑🏻‍💻`)
     console.log('He/She is a web developer')
 }
-if (inputAge>45)
+if (userAge>45)
 {
-    console.log(`${inputName}  is retired!`)
+    console.log(`${userName}  is retired!`)
 }
 
 console.log('Program Ended!!!!!');
