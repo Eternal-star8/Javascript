133b4e1b14a164a9ef723b3e4191f03006574325
Priya Mehta <pmehta12122005@gmail.com>
2025-06-19 16:53:16 +0530

    Ternary Operator

---
+++ Revision/ep-16-ternary-operator/script.js
@@ -0,0 +1,5 @@
+const username = 5>2 ? 'Anurag': 'Procodrr'
+
+const gender = 'F'
+const userMessage = (`${gender.toLowerCase === 'f' ? 'She' : 'He'} is a college student.`)
+console.log(userMessage);
